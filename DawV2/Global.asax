60acf73ddc7c45459e58d90733625ed11900a9b3
Cociobanu Victor-Andrei <62379953+Vikcoc@users.noreply.github.com>
2020-12-05 16:53:05 +0200

    Broken commit

---
+++ DawV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DawV2.MvcApplication" Language="C#" %>
---
+++ DawV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DawV2.Startup))]
+namespace DawV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
