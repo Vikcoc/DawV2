054366182f0a78bb9987ce4059bb8b4deed80deb
Cociobanu Victor-Andrei <62379953+Vikcoc@users.noreply.github.com>
2020-12-05 18:05:06 +0200

    database seems to work

---
+++ DawV2/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace DawV2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DawV2.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DawV2.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- DawV2/Models/IdentityModels.cs
+++ DawV2/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -16,18 +17,60 @@ namespace DawV2.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+        public virtual ICollection<UserGroup> UserGroups { get; set; }
+        public virtual ICollection<GroupMessage> GroupMessages { get; set; }
+        public virtual ICollection<Notice> Notices { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+        public virtual ICollection<FriendshipRequest> Requested { get; set; }
+        public virtual ICollection<FriendshipRequest> Received { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DBConnectionString", throwIfV1Schema: false)
         {
         }
+
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<FriendshipRequest> FriendshipRequests { get; set; }
+        public virtual DbSet<Group> Groups { get; set; }
+        public virtual DbSet<GroupMessage> GroupMessages { get; set; }
+        public virtual DbSet<Notice> Notices { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<UserGroup> UserGroups { get; set; }
 
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
         }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Entity<Comment>()
+                .HasOptional(x => x.ApplicationUser)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+            modelBuilder.Entity<FriendshipRequest>()
+                .HasOptional(x => x.Receiver)
+                .WithMany(x => x.Received)
+                .WillCascadeOnDelete(false);
+            modelBuilder.Entity<FriendshipRequest>()
+                .HasRequired(x => x.Requester)
+                .WithMany(x => x.Requested)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<IdentityUserLogin>()
+                .HasKey(l => new { l.LoginProvider, l.ProviderKey, l.UserId })
+                .ToTable("AspNetUserLogins");
+
+            modelBuilder.Entity<IdentityUserRole>()
+                .HasKey(r => new { r.UserId, r.RoleId })
+                .ToTable("AspNetUserRoles");
+        }
+
     }
 }
