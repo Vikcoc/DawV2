b0526146d4bad202384fae671e80338ba7740162
Cociobanu Victor-Andrei <62379953+Vikcoc@users.noreply.github.com>
2020-12-12 19:02:58 +0200

    Intermediary

---
+++ DawV2/Controllers/NoticeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DawV2.Models;
+
+namespace DawV2.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class NoticeController : Controller
+    {
+        // GET: Notice
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New(string userId)
+        {
+            var user = _db.Users.Find(userId);
+            if (user != null)
+                ViewBag.UserId = userId;
+            return View(new Notice{ApplicationUserId = userId});
+        }
+
+        [HttpPost]
+        public ActionResult New(Notice notice)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View(notice);
+                _db.Notices.Add(notice);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return View(notice);
+            }
+        }
+    }
+}
