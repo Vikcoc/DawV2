0a0ffcdc8e8907065c9ba0534847cf6654b7bf22
Cociobanu Victor-Andrei <62379953+Vikcoc@users.noreply.github.com>
2020-12-12 16:44:58 +0200

    everything

--- DawV2/App_Start/RouteConfig.cs
+++ DawV2/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DawV2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "FriendshipRequests", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Posts", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ DawV2/Controllers/CommentsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DawV2.Models;
+
+namespace DawV2.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+
+
+        [HttpPost]
+        public ActionResult New(Comment comment)
+        {
+            comment.ApplicationUserId = _db.Users.First().Id;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Comments.Add(comment);
+                    _db.SaveChanges();
+                    return Redirect("/Posts/Show/" + comment.PostId);
+                }
+                else
+                {
+                    return Redirect("/Posts/Show/" + comment.PostId);
+                }
+
+            }
+            catch (Exception e)
+            {
+                return Redirect("/Posts/Show/" + comment.PostId);
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            var comment = _db.Comments.Find(id);
+            _db.Comments.Remove(comment);
+            _db.SaveChanges();
+            return Redirect("/Posts/Show/" + comment.PostId);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Comment comment = _db.Comments.Find(id);
+            return View(comment);
+        }
+
+        [HttpPut]
+        public ActionResult Edit(int id, Comment comment)
+        {
+            try
+            {
+                var oldComment = _db.Comments.Find(id);
+                if (TryUpdateModel(oldComment))
+                {
+                    oldComment.Content = comment.Content;
+                    _db.SaveChanges();
+                }
+
+                return Redirect("/Posts/Show/" + comment.PostId);
+            }
+            catch (Exception e)
+            {
+                return Redirect("/Posts/Show/" + comment.PostId);
+            }
+        }
+    }
+}
