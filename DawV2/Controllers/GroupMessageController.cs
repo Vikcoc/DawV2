1d2ddef0dee8606aa969b8644d2c1071b31c2b0a
Cociobanu Victor-Andrei <62379953+Vikcoc@users.noreply.github.com>
2020-12-14 23:42:01 +0200

    ugly but functional

--- DawV2/Controllers/GroupMessageController.cs
+++ DawV2/Controllers/GroupMessageController.cs
@@ -11,36 +11,36 @@ namespace DawV2.Controllers
 
 
         [HttpPost]
-        public ActionResult New(GroupMessage message)
+        public ActionResult New(GroupMessage groupMessage)
         {
-            message.ApplicationUserId = User.Identity.GetUserId();
+            groupMessage.ApplicationUserId = User.Identity.GetUserId();
             try
             {
                 if (!ModelState.IsValid) 
-                    return Redirect("/Groups/Show/" + message.GroupId);
+                    return Redirect("/Groups/Show/" + groupMessage.GroupId);
                 
-                _db.GroupMessages.Add(message);
+                _db.GroupMessages.Add(groupMessage);
                 _db.SaveChanges();
 
-                return Redirect("/Groups/Show/" + message.GroupId);
+                return Redirect("/Groups/Show/" + groupMessage.GroupId);
 
             }
             catch (Exception)
             {
-                return Redirect("/Groups/Show/" + message.GroupId);
+                return Redirect("/Groups/Show/" + groupMessage.GroupId);
             }
         }
 
         [HttpDelete]
         public ActionResult Delete(int id)
         {
-            var comment = _db.Comments.Find(id);
-            if (comment == null)
+            var message = _db.GroupMessages.Find(id);
+            if (message == null)
                 return Redirect("/Groups");
 
-            _db.Comments.Remove(comment);
+            _db.GroupMessages.Remove(message);
             _db.SaveChanges();
-            return Redirect("/Groups/Show/" + comment.PostId);
+            return Redirect("/Groups/Show/" + message.GroupId);
         }
 
         public ActionResult Edit(int id)
@@ -54,11 +54,11 @@ namespace DawV2.Controllers
         {
             try
             {
-                var oldMessage = _db.Comments.Find(id);
+                var oldMessage = _db.GroupMessages.Find(id);
                 if (!TryUpdateModel(oldMessage) || oldMessage == null)
                     return Redirect("/Groups/Show/" + groupMessage.GroupId);
 
-                oldMessage.Content = groupMessage.Message;
+                oldMessage.Message = groupMessage.Message;
                 _db.SaveChanges();
 
                 return Redirect("/Groups/Show/" + groupMessage.GroupId);
