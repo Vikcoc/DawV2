3db2a27589e0cf3dfc1ac9b7505d31ed94d541f7
Victor Cociobanu <victor.cociobanu@cst.ro>
2021-01-01 23:13:05 +0200

    it's so ugly and i love it

---
+++ DawV2/Scripts/NoticeButton.js
@@ -0,0 +1,15 @@
+﻿var zin = 0;
+function myFunction(val) {
+    var id = "myDIV" + val;
+    //console.log(id);
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+        x.style.display = "inline-block";
+        zin++;
+        x.style.zIndex = zin;
+    } else {
+        x.style.display = "none";
+        x.style.zIndex = 0;
+        //zin--;
+    }
+}
